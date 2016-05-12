7040de8a8468c5bea76fef1d8eb91db4e0610b6e
Andrée Ekroth <andree.ekroth@gmail.com>
2016-05-12 19:44:27 +0200

    examples/hello_arrayfire: modify

--- examples/hello_arrayfire.rs
+++ examples/hello_arrayfire.rs
@@ -1,14 +1,24 @@
 extern crate arrayfire as af;
 
-#[allow(unused_must_use)]
 fn main() {
+    af::info().unwrap();
+
     let num_rows: u64 = 5;
-    let num_cols: u64 = 3;
+    let num_cols: u64 = 5;
     let dims = af::Dim4::new(&[num_rows, num_cols, 1, 1]);
-    println!("Create a 5-by-3 matrix of random floats on the GPU");
-    let a = match af::randu(dims, af::Aftype::F32) {
-        Ok(value) => value,
-        Err(error) => panic!("{}", error),
-    };
-    af::print(&a);
+
+    println!("Multiple random {}-by-{} matrix of random floats with identity on the GPU",
+             num_rows, num_cols);
+
+    let a = af::identity(dims, af::Aftype::F32);
+    let b = af::randu(dims, af::Aftype::F32);
+    let c = a.and_then(|a| {
+        try!(af::print(&a));
+        b.and_then(|b| {
+            try!(af::print(&b));
+            af::matmul(&a, &b, af::MatProp::NONE, af::MatProp::NONE)
+        })
+    }).unwrap();
+
+    af::print(&c).unwrap();
 }
