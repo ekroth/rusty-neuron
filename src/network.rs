d79782934a3e80cce7609a8472a9a671602c3c2a
Andrée Ekroth <andree.ekroth@gmail.com>
2016-05-17 12:43:50 +0200

    src/network: use af types

--- src/network.rs
+++ src/network.rs
@@ -2,15 +2,15 @@ extern crate arrayfire as af;
 
 pub struct Network;
 
-use self::af::{Array, AfError};
+use self::af::{Array, AfError, Aftype, Dim4};
 
 impl Network {
     // create biases from an Iterator of sizes
     fn create_biases<T>(sizes: T) -> Result<Vec<Array>, AfError>
         where T: IntoIterator<Item=u64>
     {
-        let dims = |y| af::Dim4::new(&[y, 1, 1, 1]);
-        let rxs = sizes.into_iter().map(|y| af::randu(dims(y), af::Aftype::F32));
+        let dims = |y| Dim4::new(&[y, 1, 1, 1]);
+        let rxs = sizes.into_iter().map(|y| af::randu(dims(y), Aftype::F32));
         ::util::result_sequence(rxs)
     }
 }
