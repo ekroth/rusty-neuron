0cd51e4c0d2fdf3d761e31a32c224b1e32cbc73f
Andrée Ekroth <andree.ekroth@gmail.com>
2016-05-17 11:35:30 +0200

    src/util: type annotate tests

--- src/util.rs
+++ src/util.rs
@@ -11,40 +11,45 @@ pub fn result_sequence<T, U, E>(items: T) -> Result<Vec<U>, E>
         rxs = Ok(xs);
     }
 
-    rxs
+    let r = try!(rxs);
+
+    Ok(B::from_iter(r.into_iter()))
 }
 
 #[test]
 fn sequence() {
+    type VRS = Vec<Result<u32, i32>>;
+    type RVS = Result<Vec<u32>, i32>;
+
     // empty vec should remain empty
     {
-        let xrs: Vec<Result<u32, i32>> = vec![];
-        let rxs = Ok(vec![]);
-        let orxs = result_sequence(xrs);
+        let xrs: VRS = vec![];
+        let rxs: RVS = Ok(vec![]);
+        let orxs: RVS = result_sequence(xrs);
         assert_eq!(rxs, orxs);
     }
 
     // non-empty should be the same
     {
-        let xrs: Vec<Result<u32, i32>> = vec![Ok(1), Ok(2)];
-        let rxs = Ok(vec![1, 2]);
-        let orxs = result_sequence(xrs);
+        let xrs: VRS = vec![Ok(1), Ok(2)];
+        let rxs: RVS = Ok(vec![1, 2]);
+        let orxs: RVS = result_sequence(xrs);
         assert_eq!(rxs, orxs);
     }
 
     // err should be err
     {
-        let xrs: Vec<Result<u32, i32>> = vec![Err(-1)];
-        let rxs = Err(-1);
-        let orxs = result_sequence(xrs);
+        let xrs: VRS = vec![Err(-1)];
+        let rxs: RVS = Err(-1);
+        let orxs: RVS = result_sequence(xrs);
         assert_eq!(rxs, orxs);
     }
 
     // err should propagate
     {
-        let xrs: Vec<Result<u32, i32>> = vec![Ok(1), Err(-1), Ok(2)];
-        let rxs = Err(-1);
-        let orxs = result_sequence(xrs);
+        let xrs: VRS = vec![Ok(1), Err(-1), Ok(2)];
+        let rxs: RVS = Err(-1);
+        let orxs: RVS = result_sequence(xrs);
         assert_eq!(rxs, orxs);
     }
 }
